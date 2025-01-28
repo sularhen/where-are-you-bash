f92b3e4b4b4e7977049a6a347c2a128c1ed65d97
Henry Sulbaran <104237292+sularhen@users.noreply.github.com>
2025-01-28 13:44:37 -0300

    Update whereareyou.sh

--- whereareyou.sh
+++ whereareyou.sh
@@ -1,17 +1,17 @@
                                             
-# Verificar si la respuesta contiene datos válidos
+# Verificar si la respuesta contiene datos válidos | Verifyng if the answer have valid data
 if echo "$response" | grep -q "\"status\":\"fail\""; then
   echo "No se pudo obtener información para la IP: $IP"
   exit 1
 fi
 
-# Extraer información relevante de la respuesta JSON
+# Extraer información relevante de la respuesta JSON | extract relevant information about JSON answer
 country=$(echo "$response" | grep -oP '"country":"\K[^"]+')
 region=$(echo "$response" | grep -oP '"regionName":"\K[^"]+')
 city=$(echo "$response" | grep -oP '"city":"\K[^"]+')
 isp=$(echo "$response" | grep -oP '"isp":"\K[^"]+')
 
-# Mostrar la información
+# Mostrar la información | show the info
 echo "Información para la IP: $IP"
 echo "País: $country"
 echo "Región: $region"
